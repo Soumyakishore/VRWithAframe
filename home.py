63b6cc5befb61ea5fdc6923cc107eb285ebc9b23
rupin <rupin.chheda@gmail.com>
2018-12-26 23:24:29 +0530

    Update home.py

--- home.py
+++ home.py
@@ -13,5 +13,5 @@ def projects():
     return render_template("index.html", title = 'Projects')
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run(host = '0.0.0.0',port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port,debug=True)
