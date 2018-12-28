a85a4c3dc71f9a9b5d26da77eec77cd48598c238
rupin <rupin.chheda@gmail.com>
2018-12-28 12:26:38 +0530

    Initial Import

--- home.py
+++ home.py
@@ -10,8 +10,12 @@ def hello():
 
 
 @app.route('/class1')
-def projects():
-    return render_template("index.html", title = 'Projects')
+def class1():
+    return render_template("class1-click.html")
+
+@app.route('/class2')
+def class2():
+    return render_template("class2-animation.html")
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
