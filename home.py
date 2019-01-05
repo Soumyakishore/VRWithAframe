2dba30b73bf9442d15fb408f0a5db896b600f134
rupin <rupin.chheda@gmail.com>
2019-01-05 18:38:26 +0530

    Class3

--- home.py
+++ home.py
@@ -21,6 +21,13 @@ def class2():
 def class3():
     return render_template("class3-models.html")
 
+@app.route('/class4')
+def class4():
+    return render_template("class4-physics.html")
+@app.route('/class5')
+def class5():
+    return render_template("class5-grabbable.html")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port,debug=True)
